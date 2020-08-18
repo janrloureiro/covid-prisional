83c3c764ad18ba0043e4e022c26b331704fb1056
Janaína Loureiro <janrloureiro@gmail.com>
2020-08-18 10:37:16 -0400

    inicial

---
+++ scripts/_components.js
@@ -0,0 +1,31 @@
+﻿$().ready(function () {
+    $('img[data-magnify-src]:visible').magnify();
+
+    $(".accordion > .body").toggle();
+    $(".accordion").on("click", function () {
+        $(this).toggleClass("expanded");
+        $($(this).find(".body")).slideToggle("fast", function () {
+            // Animation complete.
+        });
+    });
+
+    $("nav .prev").on("click", function () {
+        var active = $(this).parent().find("li.active");
+        var target = active.prev();
+        if (target.find("a").length > 0) {
+            var href = target.find("a").attr("href");
+            top.location.href = href;
+        }
+    });
+    $("nav .next").on("click", function () {
+        var active = $(this).parent().find("li.active");
+        var target = active.next();
+        if (target.find("a").length > 0) {
+            var href = target.find("a").attr("href");
+            top.location.href = href;
+        }
+    });
+    $(".container>nav>div>label").on("click", function () {
+        $(".container>nav>div ul").toggleClass('visible');
+    });
+});
